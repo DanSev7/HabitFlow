2c7778bb80119ba2dd29c7a96a0b63d45681ea45
DanSev7 <danielayele077@gmail.com>
2026-01-22 23:59:20 +0300

    updated icon image

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,23 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center">
-      <Text className="text-2xl font-bold">Welcome to HabitFlow!</Text>
-    </SafeAreaView> 
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 items-start">
+        <Text className="text-lg font-bold ml-4">
+          Today
+        </Text>
+        <Text className="ml-4">
+          3 tasks
+        </Text>
+        <View className="items-end">
+          <Text>1</Text>
+        </View>
+      </View>
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-2xl font-bold">Welcome to HabitFlow!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
